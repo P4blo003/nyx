fafa4c41688697173aee53f29773b8ad425713b0
P4blo003 <pablogonzalezgarcia003@gmail.com>
2026-02-20 10:52:18 +0100

    delete (nyx): Delete files for refactoring.

--- nyx/services/ai-service/src/main.py
+++ nyx/services/ai-service/src/main.py
@@ -1,7 +1,7 @@
 # ==========================================================================================
 # Author: Pablo González García.
 # Created: 23/01/2026
-# Last edited: 16/02/2026
+# Last edited: 18/02/2026
 # ==========================================================================================
 
 
@@ -10,104 +10,49 @@
 # ==============================
 
 # Standard:
-import sys
-import signal
 import asyncio
-import logging
-from typing import Optional
-from typing import Dict
-from pathlib import Path
+from argparse import ArgumentParser, Namespace
 
-# Internal:
-from application.services.client_service import AsyncClientService
-from domain.ports.client import IAsyncClientService, IAsyncClient
-from infrastructure.grpc.server import GrpcServer
-from infrastructure.triton.config.base import TritonConfig
-from infrastructure.triton.config.task import TritonTask
-from infrastructure.triton.client.grpc import GrpcAsyncClient
-from shared.utilities import logging as logging_config
-from shared.utilities import yaml
+
+# ==============================
+# PROPERTIES
+# ==============================
+
+# Server configuration.
+DEFAULT_HOST:str = "[::]"
+DEFAULT_PORT:int = 8002
 
 
 # ==============================
 # FUNCTIONS
 # ==============================
 
-async def main() -> None:
+def parse_arguments() -> Namespace:
     """
-    
     """
 
-    # Initializes logging configuration.
-    logging_config.setup()
+    parser:ArgumentParser = ArgumentParser()
 
-    # Gets core application logger.
-    log:logging.Logger = logging.getLogger("app")
+    parser.add_argument(
+        "--host",
+        type=str,
+        default=DEFAULT_HOST,
+        help=""
+    )
+    parser.add_argument(
+        "--port",
+        type=int,
+        default=DEFAULT_PORT,
+        help=""
+    )
 
-    # Wait for termination signals.
-    stop_event:asyncio.Event = asyncio.Event()
+    return parser.parse_args()
 
-    def _signal_handler():
-        stop_event.set()
+async def main(args:Namespace) -> None:
+    """
+    """
 
-    if sys.platform != "win32":
-        # Linux / macOS: Use signal handlers.
-        loop:asyncio.AbstractEventLoop = asyncio.get_running_loop()
-        loop.add_signal_handler(signal.SIGINT, _signal_handler)
-        loop.add_signal_handler(signal.SIGTERM, _signal_handler)
-
-    # Prints information.
-    log.debug("Loading configuration ...")
-    
-    # Loads configuration.
-    triton_config:TritonConfig = TritonConfig(**yaml.load_data(path=Path("./config").joinpath("triton.config.yaml")))
-
-    # Prints information.
-    log.debug("Initializing clients and services ...")
-
-    clients:Dict[str, IAsyncClient] = {}
-    for name, connection in triton_config.connections.items():
-        clients[name] = GrpcAsyncClient(
-            host=connection.host,
-            port=connection.port,
-        )
-
-    tasks:Dict[str, TritonTask] = {}
-    for name, task in triton_config.tasks.items():
-        if task.connection not in clients:
-            log.error(f"Endpoint '{task.connection}' for task '{name}' not found in the configured endpoints. Task '{name}' will be skipped.")
-        
-        else:
-            tasks[name] = task
-            log.debug(f"Task '{name}' assigned to endpoint '{task.connection}' with model '{task.model_name}'.")
-
-    client_service:IAsyncClientService = AsyncClientService[IAsyncClient](clients=clients)
-    grpc_server:GrpcServer = GrpcServer(client_service=client_service, tasks=tasks)
-    server_task:Optional[asyncio.Task] = None
-
-    try:
-
-        log.debug("Starting AI-Service ...")
-        
-        server_task = asyncio.create_task(grpc_server.start())
-
-        await stop_event.wait()
-
-    except (KeyboardInterrupt, asyncio.CancelledError): pass
-
-    finally:
-
-        log.debug("Shutting down ...")
-
-        await grpc_server.stop()
-        
-        if server_task:
-            server_task.cancel()
-            try:
-                await server_task
-            except asyncio.CancelledError: pass
-    
-    log.debug("AI-Service stopped successfully.")
+    pass
 
 
 # ==============================
@@ -116,5 +61,5 @@ async def main() -> None:
 
 if __name__ == "__main__":
 
-    # Runt async main.
-    asyncio.run(main=main())
+    # Run main.
+    asyncio.run(main=main(args=parse_arguments()))
